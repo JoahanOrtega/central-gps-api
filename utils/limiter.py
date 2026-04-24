b232ba6a0a3034d9e2a7548d5a995616f7af1e3c
Johan Ortega <joahangab23@gmail.com>
2026-04-24 15:21:46 -0600

    fix(limiter): aumentar el numero de peticiones a 1000

--- utils/limiter.py
+++ utils/limiter.py
@@ -18,5 +18,5 @@ from flask_limiter.util import get_remote_address
 
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["200 per hour"],
+    default_limits=["1000 per hour"],
 )
